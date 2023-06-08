d27c500842a469c57e2f313001bf11b87d8f2457
Vikash Basant <basantvikash360@gmail.com>
2023-06-08 16:59:41 +0530

    aswome project of ReactJS project

--- src/index.js
+++ src/index.js
@@ -1,17 +1,53 @@
+// var React = require('react');
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+// var ReactDOM = require('react-dom');
 
+
+// ReactDOM.render('kya dikhnaa hai', 'khana dikhnaa hai', 'callback function')
+
+// In React Version v16 it's poossible for render()
+// to return an array of elements
+const arr = ["vikash ", "neha ", "maa ", "papa "]
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+  // Now this array acts as an one jsx element in the 
+  // react version 16 or more update version of the react.
+  // this is nothing array of the jsx elements.
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+  /*[
+    <h1>Vikky Basant!</h1>,
+    <p>Hello World!</p>,
+    <h4>Bhagwatpur,Sarairanjan</h4>
+  ]*/
+
+  // <div>
+  //   <h1>Hello! Vikash How are you</h1>
+  //   <p>I am from Samastipur Bihar</p>
+  //   <h3>And I'm born and brodup in Bihar Itself.</h3>
+  // </div>
+
+  // // React Fragment 
+  // <React.Fragment>
+  //   <h1>Hello! Vikash How are you</h1>
+  //   <p>I am from Samastipur Bihar</p>
+  //   <h3>And I'm born and brodup in Bihar Itself.</h3>
+  // </React.Fragment>
+  
+
+  // this is also react fragement:
+  <>
+    <h1>Hello! Vikash How are you</h1>
+    <p>I am from Samastipur Bihar</p>
+    <h3>And I'm born and brodup in Bihar Itself.</h3>
+  </>
+  
+  ,document.getElementById('root'));
+
+  // ReactDOM.render( React.createElement("h1", null, "Prince Sexana"), 
+  // document.getElementById('root'));
+
+
+  // var h1 = document.createElement('h1');
+  // h1.innerHTML = "Vikash Basant";
+  // document.getElementById("root").appendChild(h1);
+
